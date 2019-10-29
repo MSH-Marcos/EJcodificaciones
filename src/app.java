94aba9d7af85a8987eb0ff3eb6f0f311b605fa5d
MSH-Marcos <marcos.solaz.hernandez@gmail.com>
2019-10-29 16:02:10 +0100

    resolucion

--- src/app.java
+++ src/app.java
@@ -1,8 +1,18 @@
+import clases.codificador;
+
 import java.util.Scanner;
 
 public class app {
 
     public static void main(String[] args) {
-        System.out.println("hola");
+        String frase = "La Operación Regenbogen ha sido todo un éxito, desplazarse mañana dia 25 al puerto de Bremen";
+
+        String codigo = codificador.codificar(frase);
+
+        System.out.println(codigo);
+
+/*        codigo = codificador.decodificar(codigo);
+
+        System.out.println(codigo);*/
     }
 }
--- src/clases/codificador.java
+++ src/clases/codificador.java
@@ -1,4 +1,30 @@
 package clases;
 
 public class codificador {
+
+    public static String codificar(String texto) {
+        char[] array = texto.toCharArray();
+        String resultado = "";
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (char)((int)array[i] * 2 - 3);
+        }
+
+        resultado = new String(array);
+
+        return resultado;
+    }
+
+/*    public static String decodificar(String texto) {
+        char[] array = texto.toCharArray();
+        String resultado = "";
+
+        for (int i = 0; i < array.length; i++) {
+            array[i] = (char)(((int)array[i] + 3)/2);
+        }
+
+        resultado = new String(array);
+
+        return resultado;
+    }*/
 }
